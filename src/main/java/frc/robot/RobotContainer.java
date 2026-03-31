c78f1d219dbff86237ec025e9f66814b2b86454c
yaghtinpouyan <132679813+yaghtinpouyan@users.noreply.github.com>
2026-03-30 22:20:46 -0400

    Simplify swerve subsystem to empty template

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,13 +3,12 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.swerve.Swerve;
-import frc.robot.subsystems.swerve.SwerveIO;
 
 public class RobotContainer {
   private final Swerve swerve;
 
   public RobotContainer() {
-    swerve = new Swerve(new SwerveIO() {});
+    swerve = new Swerve();
     configureBindings();
   }
 
--- src/main/java/frc/robot/subsystems/swerve/Swerve.java
+++ src/main/java/frc/robot/subsystems/swerve/Swerve.java
@@ -4,28 +4,11 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
 
 public class Swerve extends SubsystemBase {
-  private final SwerveIO io;
-  private final SwerveIOInputsAutoLogged inputs = new SwerveIOInputsAutoLogged();
-
-  public Swerve(SwerveIO io) {
-    this.io = io;
-  }
+  public Swerve() {}
 
   @Override
   public void periodic() {
-    io.updateInputs(inputs);
-    Logger.processInputs("Swerve", inputs);
-  }
-
-  public void drive(double xSpeedMetersPerSecond, double ySpeedMetersPerSecond, double omegaRadiansPerSecond) {
-    io.drive(xSpeedMetersPerSecond, ySpeedMetersPerSecond, omegaRadiansPerSecond);
-    Logger.recordOutput("Swerve/CommandedXSpeedMps", xSpeedMetersPerSecond);
-    Logger.recordOutput("Swerve/CommandedYSpeedMps", ySpeedMetersPerSecond);
-    Logger.recordOutput("Swerve/CommandedOmegaRadPerSec", omegaRadiansPerSecond);
-  }
-
-  public void stop() {
-    io.stop();
-    Logger.recordOutput("Swerve/Stopped", true);
+    // Template subsystem: add periodic logic and logging here as needed.
+    // Example: Logger.recordOutput("Swerve/Example", 0);
   }
 }
